f16df95dc497d2fa7759e01586508a3886a85f05
JJuanC <camiloflstudio@gmail.com>
2020-08-19 22:14:10 -0500

    resolviendo ejercicio2

---
+++ 2.Fundamentos de programacion-Parte Dos/Ejercicio2/main.js
@@ -0,0 +1,11 @@
+const superWave = document.querySelector('.super-wawe');
+
+superWave.classList.add(".hidden");
+
+const estilo = document.querySelector(".super-wawe");
+
+const color = "<strong>OLA!</strong>"
+
+estilo.innerHTML = color; 
+
+

30685e6fb20549c18ea3115c2c74ca3984df08e7
JJuanC <camiloflstudio@gmail.com>
2020-08-19 21:53:25 -0500

    resolviendo ejercicio1

---
+++ 1.Fundamentod de programacion-Parte Uno/Ejercicio8/main.js
@@ -0,0 +1,10 @@
+const sectionB = document.querySelector('.section-b');
+
+sectionB.classList.add('hidden');
+
+
+
+const segundo = document.querySelector(".boton2");
+
+segundo.classList.add("hidden");
+
---
+++ 2.Fundamentos de programacion-Parte Dos/Ejercicio1/main.js
@@ -0,0 +1,11 @@
+console.log("Hola mundo");
+
+const age = 56;
+console.log(age);
+
+const welcomeParagraph = document.querySelector('.welcomeText');
+
+// Logueo el contenido de la constante welcomeParagraph en la consola y compruebo que tiene asignado el elemento de HTML que espero antes de cambiar su contenido
+console.log('welcomeParagraph: ', welcomeParagraph);
+
+welcomeParagraph.innerHTML = 'Bienvenido a Academia Geek';

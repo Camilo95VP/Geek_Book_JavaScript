53aca16594b282c98583984687c8367c0d0bfd77
JJuanC <camiloflstudio@gmail.com>
2020-08-19 20:43:05 -0500

    resolviendo ejercicio4

---
+++ 1.Fundamentod de programacion-Parte Uno/Ejercicio4/main.js
@@ -0,0 +1,5 @@
+var seleccionado = document.querySelector(".title");
+seleccionado.innerHTML += " " + "juan"
+
+//var seleccionado = document.querySelector(".title");
+//seleccionado.innerHTML += " " + "Pedro"

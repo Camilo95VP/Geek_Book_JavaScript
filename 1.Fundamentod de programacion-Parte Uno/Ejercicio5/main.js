3afd211843d4d1723e0e94274ee945f7a1fa29ce
JJuanC <camiloflstudio@gmail.com>
2020-08-19 21:10:08 -0500

    resolviendo ejercicio5 y 6

---
+++ 1.Fundamentod de programacion-Parte Uno/Ejercicio5/main.js
@@ -0,0 +1,5 @@
+const listElement = document.querySelector('.list');
+
+const content = '<li><a href="#">Home</a></li><li><a href="#">Contact</a></li>';
+
+listElement.innerHTML = content;
---
+++ 1.Fundamentod de programacion-Parte Uno/Ejercicio6/main.js
@@ -0,0 +1,9 @@
+const contenido = document.querySelector(".contenedor");
+
+const inside = "<h1>Lorem Ipsum</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>";
+
+const imagen = document.querySelector(".contenedor");
+
+const insert = "<li>HOLA A TODOS</li>";
+
+contenido.innerHTML = inside + insert; 

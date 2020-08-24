a9b99d9c616379f696aeae532599103aa6e2bf50
JJuanC <camiloflstudio@gmail.com>
2020-08-23 21:42:09 -0500

    terminando sesion6

--- 6.Objetos/Ejercicio7/main.js
+++ 6.Objetos/Ejercicio7/main.js
@@ -1,7 +1,20 @@
-const job =  {
-    firstName: "juan",
-    lastName: "castro",
-    age: 20,
-    job = job,
+const usuario={};
 
-}
+const job="developer";
+
+usuario.firstName="Juan";
+usuario.lastName="Castañeda";
+usuario.age=25;
+usuario.job=job;
+
+
+console.log(`Mi nombre y mi apellido son  ${usuario.firstName} ${usuario.lastName}, tengo ${usuario.age} años y soy
+${usuario.job}`);
+
+
+
+usuario.firstName="keychron";
+usuario.age=27;
+
+console.log(`Mi nombre y mi apellido son  ${usuario.firstName} ${usuario.lastName}, tengo ${usuario.age} años y soy
+${usuario.job}`);
